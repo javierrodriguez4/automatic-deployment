b8c57b20ce9d95d4d6feb524c14fe1bb41621a8e
javierrodriguez4 <javierrodriguez4@outlook.com>
2024-07-30 16:15:39 -0300

    mejoras script 2 mite

--- mite1x/mite1x-installer-2.ps1
+++ mite1x/mite1x-installer-2.ps1
@@ -41,8 +41,42 @@ $exePath = Join-Path -Path (Split-Path -Parent $driversPath) -ChildPath "MitE1x.
 
 # Ejecutar MitE1x.exe
 Write-Output "Ejecutando $exePath..."
-Start-Process -FilePath $exePath -Wait -NoNewWindow
+$process = Start-Process -FilePath $exePath -PassThru
+
+# Esperar a que MitE1x.exe inicie (esto puede necesitar ajustarse dependiendo del tiempo de inicio de la aplicación)
+Start-Sleep -Seconds 10
+
+# Cerrar MitE1x.exe
+Write-Output "Cerrando $exePath..."
+$process | Stop-Process -Force
+
+# Obtener IP de la variable ACD_IP del archivo .env
+$acdIp = $envVariables["ACD_IP"]
+
+# Obtener IP de la interfaz de red de Windows 10
+$networkIp = (Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.Address -ne "127.0.0.1" }).IPAddress
+
+# Ruta al archivo sttr_E1.rwe
+$rweFilePath = "P:\Servicios\MitE1x\cfg\sttr_E1.rwe"
+
+# Leer el contenido del archivo
+$content = Get-Content -Path $rweFilePath
+
+# Reemplazar las líneas específicas
+$content = $content -replace "LoggingDir=P:\\Servicios\\MitE1x\\Logging\\", "LoggingDir=L:\\Servicios\\MitE1x\\Logging\\"
+$content = $content -replace "Host=127.0.0.1", "Host=$acdIp"
+$content = $content -replace "IpSrc=.*", "IpSrc=$networkIp"
+
+# Guardar el archivo con las modificaciones
+Set-Content -Path $rweFilePath -Value $content
 
 # Verificar éxito y mostrar mensaje emergente
 Add-Type -AssemblyName System.Windows.Forms
-[System.Windows.Forms.MessageBox]::Show("El archivo install.bat y MitE1x.exe se ejecutaron correctamente.", "Ejecución Exitosa", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+[System.Windows.Forms.MessageBox]::Show("El archivo install.bat, MitE1x.exe se ejecutaron correctamente, MitE1x.exe fue cerrado y el archivo sttr_E1.rwe ha sido actualizado.", "Ejecución Exitosa", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+
+# Esperar un momento para asegurarse de que el archivo se haya guardado correctamente
+Start-Sleep -Seconds 5
+
+# Volver a iniciar MitE1x.exe
+Write-Output "Reiniciando $exePath..."
+Start-Process -FilePath $exePath -NoNewWindow
